e9b63e7aa573ae33e1d964d507fad9c1d50673dd
shiba <rp.shiba2@gmail.com>
2024-09-07 22:34:04 +0530

    header navbar fix

--- zxinfoweb/src/Components/HeaderComponent/Header.js
+++ zxinfoweb/src/Components/HeaderComponent/Header.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Topbar from "../HomeComponent/Topbar";
 import Navbar from "../HomeComponent/Navbar";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <>
-      <Topbar />
-      <Navbar />
-    </>
+    <header className="fixed top-0 left-0 right-0 z-50">
+      <div 
+        className={`bg-indigo-950 transition-all duration-300 ${
+          isScrolled ? 'h-0 overflow-hidden' : 'h-auto'
+        }`}
+      >
+        <Topbar />
+      </div>
+      <div 
+        className={`transition-all duration-300 ${
+          isScrolled ? 'bg-white shadow-md' : 'bg-indigo-950'
+        }`}
+      >
+        <Navbar isScrolled={isScrolled} />
+      </div>
+    </header>
   );
 };
 
--- zxinfoweb/src/Components/HomeComponent/Navbar.js
+++ zxinfoweb/src/Components/HomeComponent/Navbar.js
@@ -1,32 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { CiMenuFries } from "react-icons/ci";
+import { IoMdArrowDropdown } from "react-icons/io";
 import { SlClose } from "react-icons/sl";
-import { FaChevronDown } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
 
-const Navbar = () => {
+const Navbar = ({ isScrolled }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 768) {
-        setIsOpen(false);
-      }
-    };
-
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 10);
-    };
-
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
 
   const handleToggle = () => setIsOpen(!isOpen);
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
@@ -39,61 +19,54 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white shadow-md' : 'bg-transparent'}`}>
+    <nav className="transition-all duration-300">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link to="/" className="flex items-center space-x-3">
-            <img
-              src="https://i.postimg.cc/4xhX5wht/zxpinfotech-high-resolution-logo-transparent.png"
-              className="h-8 w-auto"
-              alt="ZXPINFOTECH Logo"
-            />
-            <span className={`font-bold text-xl ${isScrolled ? 'text-gray-800' : 'text-white'}`}>
+            <span className={`font-bold text-xl ${isScrolled ? 'text-indigo-950' : 'text-white'}`}>
               ZXPINFOTECH
             </span>
           </Link>
 
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              {navItems.map((item) => (
-                <NavLink
-                  key={item.name}
-                  to={item.path}
-                  className={({ isActive }) =>
-                    `px-3 py-2 rounded-md text-sm font-medium ${
-                      isActive
-                        ? 'bg-indigo-600 text-white'
-                        : `${isScrolled ? 'text-gray-800' : 'text-white'} hover:bg-indigo-500 hover:text-white`
-                    }`
-                  }
-                >
-                  {item.name}
-                </NavLink>
-              ))}
-              <div className="relative">
-                <button
-                  onClick={toggleDropdown}
-                  className={`px-3 py-2 rounded-md text-sm font-medium ${isScrolled ? 'text-gray-800' : 'text-white'} hover:bg-indigo-500 hover:text-white flex items-center`}
-                >
-                  Pages <FaChevronDown className="ml-1" />
-                </button>
-                {dropdownOpen && (
-                  <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Page 1</a>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Page 2</a>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Page 3</a>
-                    </div>
+          <div className="hidden lg:flex items-center space-x-4">
+            {navItems.map((item) => (
+              <NavLink
+                key={item.name}
+                to={item.path}
+                className={({ isActive }) =>
+                  `px-3 py-2 text-sm font-medium ${
+                    isActive
+                      ? 'text-indigo-500'
+                      : `${isScrolled ? 'text-indigo-950' : 'text-white'} hover:text-indigo-500`
+                  }`
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
+            <div className="relative">
+              <button
+                onClick={toggleDropdown}
+                className={`px-3 py-2 text-sm font-medium ${isScrolled ? 'text-indigo-950' : 'text-white'} hover:text-indigo-500 flex items-center`}
+              >
+                Pages <IoMdArrowDropdown className="ml-1" />
+              </button>
+              {dropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                  <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Page 1</a>
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Page 2</a>
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Page 3</a>
                   </div>
-                )}
-              </div>
+                </div>
+              )}
             </div>
           </div>
 
-          <div className="md:hidden">
+          <div className="lg:hidden">
             <button
               onClick={handleToggle}
-              className={`inline-flex items-center justify-center p-2 rounded-md ${isScrolled ? 'text-gray-800' : 'text-white'} hover:text-white hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white`}
+              className={`inline-flex items-center justify-center p-2 rounded-md ${isScrolled ? 'text-indigo-950' : 'text-white'} hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white`}
             >
               {isOpen ? <SlClose className="h-6 w-6" /> : <CiMenuFries className="h-6 w-6" />}
             </button>
@@ -102,8 +75,8 @@ const Navbar = () => {
       </div>
 
       {/* Mobile menu */}
-      <div className={`md:hidden ${isOpen ? 'block' : 'hidden'}`}>
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <div className={`lg:hidden ${isOpen ? 'block' : 'hidden'}`}>
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-white">
           {navItems.map((item) => (
             <NavLink
               key={item.name}
@@ -111,8 +84,8 @@ const Navbar = () => {
               className={({ isActive }) =>
                 `block px-3 py-2 rounded-md text-base font-medium ${
                   isActive
-                    ? 'bg-indigo-600 text-white'
-                    : 'text-gray-800 hover:bg-indigo-500 hover:text-white'
+                    ? 'text-indigo-500'
+                    : 'text-indigo-950 hover:text-indigo-500'
                 }`
               }
               onClick={handleToggle}
@@ -122,15 +95,15 @@ const Navbar = () => {
           ))}
           <button
             onClick={toggleDropdown}
-            className="w-full text-left px-3 py-2 rounded-md text-base font-medium text-gray-800 hover:bg-indigo-500 hover:text-white flex items-center justify-between"
+            className="w-full text-left px-3 py-2 rounded-md text-base font-medium text-indigo-950 hover:text-indigo-500 flex items-center justify-between"
           >
-            Pages <FaChevronDown />
+            Pages <IoMdArrowDropdown />
           </button>
           {dropdownOpen && (
             <div className="pl-4">
-              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 hover:bg-indigo-500 hover:text-white">Page 1</a>
-              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 hover:bg-indigo-500 hover:text-white">Page 2</a>
-              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 hover:bg-indigo-500 hover:text-white">Page 3</a>
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-indigo-950 hover:text-indigo-500">Page 1</a>
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-indigo-950 hover:text-indigo-500">Page 2</a>
+              <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-indigo-950 hover:text-indigo-500">Page 3</a>
             </div>
           )}
         </div>
